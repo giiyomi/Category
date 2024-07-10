45f7d0151fa09044f95f12a3a791b2227d53e505
Ivan Nollora <palakol80@gmail.com>
2024-07-11 02:01:34 +1000

    Create Category - Push

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -5,13 +5,21 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
-
+  setup do
+    @category = categories(:one)
+  end
 
   test "should get index" do
     get categories_url
     assert_response :success
   end
 
+  test "should create category" do
+    assert_difference("Category.count") do
+      post categories_url, params: { category: {name: @category.name} }
+    end
 
+    assert_redirected_to categories_url
+  end
 
 end
