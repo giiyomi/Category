86f8213ca85fad90e1deb6f145e83bfd40ed60b9
Ivan Nollora <palakol80@gmail.com>
2024-07-11 22:51:45 +1000

    Added a test to the 'Model' folder in the 'Test' directory to test the validation in the 'App' folder's model.

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,6 +4,15 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save category without a name" do
+    category = Category.new
+    assert_not category.save, "Saved the category without a name"
+  end
+
+  test "should save category with a unique name" do
+    category = Category.new(name: "Unique Category")
+    assert category.save, "Could not save the category with a unique name"
+  end
 
 
 end
