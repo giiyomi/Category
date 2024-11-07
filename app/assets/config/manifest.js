181d9847374af95e558c25034b2de214b654cb2a
Ivan Nollora <palakol80@gmail.com>
2024-11-07 18:46:14 +1100

    changed some designs using bootstrap and vanilla css

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link login/login.css
+//= link categories/categories.css
+//= link task/task.css
+
